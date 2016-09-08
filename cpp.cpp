29d59b7b9cd2d66aa93be346b81f971850e047dd
Dmitry Ponyatov <dponyatov@gmail.com>
2016-09-08 11:24:57 +0400

    + parser
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -1,2 +1,17 @@
 #include "hpp.hpp"
-int main() {}
+#define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
+void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
+int main() { return yyparse(); }
+
+Sym::Sym(string T, string V) { tag=T; val=V; }
+Sym::Sym(string V):Sym("sym",V){}
+
+string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
+string Sym::head() { ostringstream os;
+	os<<"<"<<tag<<":"<<val<<"> @ "<<this; return os.str(); }
+string Sym::dump(int depth) { string S = "\n"+pad(depth)+head();
+	return S; }
+
+Num::Num(string V):Sym("num",V){}
+
+Op::Op(string V):Sym("op",V){}
--- hpp.hpp
+++ hpp.hpp
@@ -2,7 +2,27 @@
 #define _H_HPP
 
 #include <iostream>
+#include <sstream>
 #include <cstdlib>
 using namespace std;
+
+struct Sym {
+	string tag,val;
+	Sym(string,string); Sym(string);
+	virtual string head(); string pad(int);
+	virtual string dump(int=0);
+};
+
+struct Num : Sym { Num(string); };
+
+struct Op : Sym { Op(string); };
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
+extern int yyparse();
+extern void yyerror(string);
+#include "ypp.tab.hpp"
 
 #endif // _H_HPP
